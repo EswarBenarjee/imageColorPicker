abd6ebd08ad8c4d3ccbb5a69679cfdde157c35d9
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-09-29 23:57:08 +0530

    chore: updating mobile view

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,25 +14,34 @@ const Body = () => {
   const [history, setHistory] = useState(["eswar.jpg"]);
 
   return (
-    <Container className="containerPad mt-5" style={{ minHeight: "80vh" }}>
-      <Row className="d-flex justify-content-center align-items-center">
-        <Col md={6}>
-          <Image
-            img={history[0]}
-            hex={hex}
-            setHex={setHex}
-            rgb={rgb}
-            setRgb={setRgb}
-          />
-        </Col>
-        <Col md={6} className="mt-5 mt-md-0 my-3">
-          <UploadImage setHistory={setHistory} />
-          <Colors hex={hex} setHex={setHex} rgb={rgb} setRgb={setRgb} />
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center align-items-center">
-        <History history={history} setHistory={setHistory} />
-      </Row>
+    <Container className={`containerPad mt-5`} style={{ minHeight: "80vh" }}>
+      {!window.EyeDropper ? (
+        <div>
+          Eyedropper does not work in Mobile Devices. Please check this project
+          in Laptop.
+        </div>
+      ) : (
+        <>
+          <Row className="d-flex justify-content-center align-items-center">
+            <Col md={6}>
+              <Image
+                img={history[0]}
+                hex={hex}
+                setHex={setHex}
+                rgb={rgb}
+                setRgb={setRgb}
+              />
+            </Col>
+            <Col md={6} className="mt-5 mt-md-0 my-3">
+              <UploadImage setHistory={setHistory} />
+              <Colors hex={hex} setHex={setHex} rgb={rgb} setRgb={setRgb} />
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center align-items-center">
+            <History history={history} setHistory={setHistory} />
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
